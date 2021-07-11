6d7ac5db295ad3778908154477dd8a8fc4cdb411
afrid83 <afridcool83@gmail.com>
2021-07-11 23:12:35 +0530

    Examples completed

--- chapter_1/TestCard.cpp
+++ chapter_1/TestCard.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 void testCard() {
     vector<CreditCard*> wallet(10);
-    wallet[0] = new CreditCard("1234", "Afrid", 123);
-    wallet[1] = new CreditCard("1235", "Asifa", 124);
-    wallet[2] = new CreditCard("1236", "Arshad", 125);
+    wallet[0] = new CreditCard("1234", "Afrid", 2500);
+    wallet[1] = new CreditCard("1235", "Asifa", 3500);
+    wallet[2] = new CreditCard("1236", "Arshad", 5000);
 
     for (int j = 1; j <= 16; j++) {
         wallet[0] -> chargeIt(double(j));
@@ -19,6 +19,7 @@ void testCard() {
     for (int i = 0; i < 3; i++) {
         cout << *wallet[i] ;
         while (wallet[i] -> getBalance() > 100.0) {
+            wallet[i] -> makePayment(100.0);
             cout << "New balance = " << wallet[i] -> getBalance() << "\n";
         }
         cout << "\n";
