1bb67f76599463aa1ae90db641f4f9c577166394
afrid18 <afridcool83@gmail.com>
2021-07-22 01:07:49 +0530

    Added Vector.h file

---
+++ chapter_2/Vector.h
@@ -0,0 +1,10 @@
+class Vector {
+    public:
+        Vector(int s) : elem{new double[s]}, sz{s}{}
+        double& operator[](int i) {return elem[i];}
+        int size() {return sz;}
+
+    private:
+        double* elem; 
+        int  sz;
+};
