ee940ce12d06f182c1f4933fd0454c952d467c15
afrid18 <afridcool83@gmail.com>
2021-09-16 11:50:57 +0530

    Worked inheritance

--- chapter_2/inheritance.cpp
+++ chapter_2/inheritance.cpp
@@ -1,17 +1,45 @@
+#include <iostream>
+
+using namespace std;
 class Person {
     private:
         int ID;
         string name;
     public:
-        Person(int ID, string name) {
-            
-        }
-        int getID() {return ID}
+        Person(int id, string name0);
+        int getID() {return ID;}
         string getname() {
             return name;
         }
-}
+};
 
-class Student : class Person {
+Person::Person(int id, string name0) :
+    ID(id),
+    name(name0) {}
+
+
+class Student : public Person {
     private:
+        string major;
+        int gradYear;
+
+    public:
+        Student(int id, string name0, string mjr, int gy);
+        int getGY() { return gradYear; }
+        string getMajor() {return major;}
+};
+
+Student::Student(int id, string name0, string mjr, int gy) :
+    Person(id, name0),
+    major(mjr),
+    gradYear(gy) {}
+
+
+int main(void) {
+    Student santosh(101, "Santosh", "CSE", 2023);
+    cout << santosh.getID() << endl;
+    cout << santosh.getname()<< endl;
+    cout << santosh.getMajor() << endl;
+    cout << santosh.getGY() << endl;
+    return 0;
 }
