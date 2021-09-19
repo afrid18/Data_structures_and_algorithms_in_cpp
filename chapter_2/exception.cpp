3c439318aea258d9d90eb6fb03ae1800d497b134
afrid18 <afridcool83@gmail.com>
2021-09-19 16:08:05 +0530

    Worked with exception

--- chapter_2/exception.cpp
+++ chapter_2/exception.cpp
@@ -17,17 +17,17 @@ class MathException {
 class zeroDivideError : public MathException {
     public:
         zeroDivideError(const string& err)
-            : errMsg(err) {}
+            : MathException(err), errMsg(err) {}
     private:
         string errMsg;
 };
 
 // NegativeRoot exception class that has inherited MathException class
 
-class NegativeRootError: public mathexception {
+class NegativeRootError: public MathException {
     public:
         NegativeRootError(const string& err)
-            : errmsg(err) {}
+            : MathException(err), errMsg(err) {}
     private:
         string errMsg;
 };
@@ -36,9 +36,9 @@ int main(void) {
     try {
         int divisor = 0;
         if (divisor == 0)
-            throw ZeroDivideError("Trying to divide by 0");
+            throw zeroDivideError("Trying to divide by 0");
     }
-    catch(ZeroDivideError zde) {
+    catch(zeroDivideError zde) {
         cout << "HAha dividing by zero 🤣" << endl;
     }
     return 0;
