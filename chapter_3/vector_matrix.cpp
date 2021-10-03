05d0dd1c46879d0d1f361d985d7a29760401184e
afrid18 <afridhussain.d@gmail.com>
2021-10-03 18:32:43 +0530

    Vector_matrix program

---
+++ chapter_3/vector_matrix.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    vector<vector<int>> M(n, vector<int>(m));
+    cout << "Matrix has been created" << endl;
+    return 0;
+}
