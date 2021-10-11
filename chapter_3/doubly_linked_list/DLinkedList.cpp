5f47d65cff7a160744e7e964102c917910889553
afrid18 <afridhussain.d@gmail.com>
2021-10-11 23:34:24 +0530

    Started Doubly linked list

---
+++ chapter_3/doubly_linked_list/DLinkedList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "DLinkedList.h"
+
+using namespace std;
+
+
+DLinkedList::DLinkedList() {
+    header = new DNode;
+    trailer = new DNode;
+    header -> next = trailer;
+    trailer -> prev = header;
+}
+
+DLinkedList::~DLinkedList() {
+    while(!empty() ) removeFront();
+    delete header;
+    delete trailer;
+}
+
+
+bool DLinkedList::empty() const {
+    return (header -> next == trailer);
+}
+
+const Ele& DLinkedList::front() const {
+    return header -> next -> ele;
+}
+
+const Ele& DLinkedList::back() const {
+    return trailer -> prev -> ele;
+}
+
+void DLinkedList::add(DNode* v, const Ele& e) {
+    DNode* u = new DNode;
+    u -> ele = e;
+    u -> next = v;
+    u -> prev = v -> prev;
+    v -> prev -> next = v -> prev = u;
+}
+
+void DLinkedList::addFront(const Ele& e) {
+    add(header -> next, e);
+}
+
+void DLinkedList::addBack(const Ele& e) {
+    add(trailer, e);
+}
+
---
+++ chapter_3/doubly_linked_list/DLinkedList.h
@@ -0,0 +1,36 @@
+#ifndef DLINKEDLIST_H
+#define DLINKEDLIST_H 1
+
+
+typedef string Ele;
+
+class DNode {
+    private:
+        Ele ele;
+        DNode* left;
+        DNode* right;
+        friend class DLinkedList;
+};
+
+class DlinkedList {
+    private:
+        DNode* header;
+        DNode* trailer;
+    protected:
+        void add(DNode* v, const Ele& e);
+        void remove(DNode* v);
+    public:
+        DLinkedList();
+        ~DLinkedList();
+        bool empty() const;
+        const Ele& front() const;
+        const Ele& back() const;
+        void addFront(const Ele& e);
+        void addBack(const Ele& e);
+        void removeFront();
+        void removeBack();
+};
+
+
+
+#endif
