a26e3be24cf08d7f1b8cd02404948b2ab835ef7d
afrid18 <afridhussain.d@gmail.com>
2021-10-15 23:04:04 +0530

    added ruler program that prints ruler

---
+++ chapter_3/recursion/ruler.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+}
+
+
+void drawOneTick(int tickLength, int tickLabel = -1) {
+    for (int i = 0; i < tickLength; i++)
+        cout << "-";
+    if (tickLael >= 0) cout << " " << tickLabel;
+    cout << "\n";
+}
+
+void drawTicks(int tickLength) {
+    if (tickLength > 0) {
+        drawTicks
+    }
+}
