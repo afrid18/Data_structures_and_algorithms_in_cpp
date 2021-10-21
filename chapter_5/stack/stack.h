8227275b8933e500a88bd66f681db306903ab6cd
afrid18 <afridhussain.d@gmail.com>
2021-10-21 23:00:51 +0530

    Not completed yet

--- chapter_5/stack/stack.h
+++ chapter_5/stack/stack.h
@@ -2,5 +2,21 @@
 #define STACK_H
 
 
+class StackEmpty : RunTimeException {  // StackEmpty exception handling class implementation
+    public:
+        StackEmpty(const string& err) : RunTimeException(err) {}
+};
+
+template <typename E>
+class Stack {
+    public:
+        int size() const;
+        bool empty() const;
+        const E& top() const throw(StackEmpty);
+        void push(const E& ele);
+        void pop() throw(StackEmpty);
+}
+
+
 
 #endif
