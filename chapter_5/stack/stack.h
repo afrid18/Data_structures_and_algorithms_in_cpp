578da9db8da01ee4cbef163fa63f32b88df70b96
afrid18 <afridhussain.d@gmail.com>
2021-11-11 10:01:35 +0530

    Started stack implementation

--- chapter_5/stack/stack.h
+++ chapter_5/stack/stack.h
@@ -1,21 +1,27 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include "./../../RuntimeException.h"
+
 
 class StackEmpty : RunTimeException {  // StackEmpty exception handling class implementation
     public:
         StackEmpty(const string& err) : RunTimeException(err) {}
 };
 
-template <typename E>
+typedef int E;
 class Stack {
     public:
+ :q
+ ls
+
+
         int size() const;
         bool empty() const;
         const E& top() const throw(StackEmpty);
         void push(const E& ele);
         void pop() throw(StackEmpty);
-}
+};
 
 
 
