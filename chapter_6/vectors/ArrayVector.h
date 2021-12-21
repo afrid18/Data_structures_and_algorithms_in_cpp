33d0b75fbbe3263fee802af6728d676aac98371e
afrid18 <afridhussain.d@gmail.com>
2021-12-21 23:59:21 +0530

    Started chapter 6

---
+++ chapter_6/vectors/ArrayVector.h
@@ -0,0 +1,13 @@
+#ifndef ARRAYVECTOR_H
+#define ARRAYVECTOR_H
+
+template <typename E>
+class ArrayVector {
+    public:
+        ArrayVector();
+        int size() const;
+        bool empty() const;
+        
+};
+
+#endif
